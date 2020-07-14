aba300e36139f14a0c2525e9ab25acb035c1d599
syrine-ben-attia <benattiasyrine@gmail.com>
2020-07-14 12:00:26 +0200

    modif page Home

--- stageReact/Home.js
+++ stageReact/Home.js
@@ -1,3 +1,26 @@
 import react from 'react'
 import { StyleSheet, View , Text , Image }  from 'react-native'
-import 
+import articleItem from './articleItem' 
+import { TextInput, FlatList } from 'react-native-gesture-handler'
+
+class Home extends ReactComponent {
+    constructor(props) {
+        super(props)
+        this.state = { articles: []
+        }   }
+
+        _loadArticles() {
+            
+        }
+        render() {
+            return ( 
+                <View style={styles.main_container}>
+                    <TextInput style={styles.textinput} placeholder="Titre d'article" />
+                    <FlatList 
+                    data={this.state.articles}
+                    keyExtractor={(item) => item.id.toString()}
+            renderItem={({item}) => <articleItem article={item} /> } /> 
+                </View>
+            )
+        }
+}
